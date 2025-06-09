4bd34cb0e878259cd014aa1ce627662f5a0bbc95
LindsellR <lindsellrichard@gmail.com>
2025-06-09 12:02:46 +1000

    Added more relevant comments along with accessibility label for text input field

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,13 @@
-// Import React and necessary components from React Native
+//Chat,js - The chat screen will eventually display the chat conversation, with an input field and submit button. It will also provide users with the ability to send images and location data that will be stored offline and online.
 
 import { useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-// Chat screen components props from react navigation
+// Chat screen components props from react navigation. The navigation is set in start.js to navigate her. The route prop sets the parameters for what will be displayed (In this case the  users name at the top of the screen and the background color  chosen by the user).
 const Chat = ({ route, navigation }) => {
   const { name, bgColor } = route.params;
 
-  //useEffect hook to set screen title to users name when the component starts
+  //useEffect hook sets the screen title to users name by using the navigation.setOptions method
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
@@ -15,7 +15,7 @@ const Chat = ({ route, navigation }) => {
   return (
     //returns container with background color chosen by user
     <View style={[styles.container, { backgroundColor: bgColor }]}>
-      {/*Placeholder for chat screen */}
+      {/*Placeholder for future chat UI component, where the chat converstion, input field and submit button will eventually display */}
       <Text style={styles.titleText}>Chat</Text>
     </View>
   );
@@ -24,11 +24,11 @@ const Chat = ({ route, navigation }) => {
 //Styling for Chat view
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, //Takes up full height and width of screen
     justifyContent: "center",
     alignItems: "center",
   },
-  // styling for pkaceholder
+  // styling for placeholder text.
   titleText: {
     fontSize: 32,
     color: "red",
--- components/Start.js
+++ components/Start.js
@@ -1,4 +1,5 @@
-// Import React and necessary components from React Native
+//Start.js - The start screen is where users will enter their name to join in the chat, choose a background color for their chat screen, and enter the chat via a "Start Chat button".
+
 import { useState } from "react";
 import {
   StyleSheet,
@@ -10,47 +11,52 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-//  Start screen component functionality
-const Start = ({ navigation }) => {
-  //State to store users name
+//  Start screen component functionality. This is the user entry point where the user enters their name and background color choice. When they navigate to the app, these valuse (name, bgColor ) are passed in as parameters.
+const Start = ({navigation}) => {
+  //State to store users name (used to store and display the users name in the Chat.js UI)
   const [name, setName] = useState("");
 
-  //State to store background color
+  //State to store background color (and display in Chat.js UI)
   const [bgColor, setBgColor] = useState("#FFFFFF");
 
-  //Load backgound image from Assets folder in root
-  const image = require("../A5-chatapp-assets/Bgnd_Image.png");
+  //This is the backgound image from Assets folder displayed on the start screen (Located in root directory)
+  const image = require("../assets/Bgnd_Image.png");
 
-  //Colors array for user chosen background color
+  //This is the colors array available for the user chosen background color
   const colors = ["#090C08", "#474056", "#8A95A5", "#B9C6AE"];
 
   return (
     <View style={styles.container}>
+
       {/*Background image on start screen */}
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        {/* App Title*/}
+
+        {/* App Title - This is a working title and depending on where it is to be used in the app, could be moved to a separate text component*/}
         <Text style={styles.titleText}>Chat App</Text>
-        {/*White background container on start page for text input, color picker and start chat button*/}
+
+        {/*White background container on start page. This is the background of the container that holds the form for text input, color picker and start chat button*/}
         <View style={styles.whiteBox}>
-          {/*Username input field with icon*/}
+
+          {/*Username input field with icon. When the user inputs their name, the text is stored in local state and displayed at the top of the chat.js*/}
           <View style={styles.inputWrapper}>
             <Image
-              source={require("../A5-chatapp-assets/icon.png")}
+              source={require("../assets/icon2.png")}
               style={styles.icon}
             />
             <TextInput
               style={styles.textInput}
               value={name}
               onChangeText={setName}
-              placeholder="Your Name"
+              placeholder="Enter Your Name"
+              placeholderTextColor="rgba(117, 112, 131, 0.5)"
+              accessibilityLabel="Enter Your Name"
             />
           </View>
 
-          {/*Background Color Picker section*/}
+          {/*Background Color Picker section - the user selects one of the pre-defined color options which is then passed as a prop to the chat screen*/}
           <View style={styles.colorSection}>
             <Text style={styles.colorLabel}>Choose Background Color</Text>
             <View style={styles.colorOptions}>
-              {/*Mapping Color array and setting user picked background color*/}
               {colors.map((color) => (
                 <TouchableOpacity
                   key={color}
@@ -70,7 +76,7 @@ const Start = ({ navigation }) => {
               ))}
             </View>
           </View>
-          {/*Button to navigate to chat screen with selected name and bground color using TouchableOpacity for more styling options*/}
+          {/*Button to navigate to chat screen. This passes the usersname and selected bground color as navigation parameters to display on the chat screen. Using TouchableOpacity instead of Button for more styling options*/}
           <TouchableOpacity
             style={styles.button}
             onPress={() =>
@@ -149,7 +155,7 @@ const styles = StyleSheet.create({
   },
 
   //Background color selection
-colorSection: {
+  colorSection: {
     width: "88%",
     alignSelf: "center",
     marginTop: 10,
